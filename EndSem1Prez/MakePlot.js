6ab281baa8a9dbdde1e94b35fe5111c16b217a2e
aarongraybill <agraybill@haverford.edu>
2021-12-07 13:53:11 -0500

    Delete extraneities

--- EndSem1Prez/MakePlot.js
+++
@@ -1,6 +0,0 @@
-var elt = document.getElementById('calculator');
-    var options = { border: false };
-    var calculator = Desmos.GraphingCalculator(elt, options);
-    calculator.setExpressions([
-      {id:'1', latex:'x^2+y^2<25'}
-    ]);
