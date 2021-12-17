dda22e9ff1098c88401fee89a7e665e42cd34eb2
aarongraybill <agraybill@haverford.edu>
2021-12-16 19:28:55 -0500

    Making progress on submission

---
+++ Experimentation/Plot2PeriodData.R
@@ -0,0 +1,30 @@
+d <- read.csv("/Users/aarongraybill/Downloads/TryData (3).csv")
+
+library(ggplot2)
+library(dplyr)
+
+d <- 
+  d %>% 
+mutate(premium=nu/nd-1)
+
+d_sum <- 
+  d %>% 
+  group_by(winner) %>% 
+  summarise(across(c(nu:I0,premium),median))
+
+
+ggplot(d)+
+  geom_point(aes(x=nd,y=log(premium),col=winner),alpha=.1)+
+  guides(colour = guide_legend(override.aes = list(alpha = 1)))+
+  #scale_color_manual(values = c("#8C2730","#255059","#FFBA3D","#13091C"))
+  scale_color_viridis_d(option="cividis")
+
+
+ggplot(d)+
+  geom_violin(aes(winner,Iu-Id)#,scale = "count"
+              )+
+  ylim(0,3)
+
+
+ggplot(d)+
+geom_density2d_filled(aes(I0,nd))
